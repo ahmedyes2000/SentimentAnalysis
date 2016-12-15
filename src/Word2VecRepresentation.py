29f84cbd7f5e609b1b8b95685db2c49437a6bd28
xyryx <ajeyatj@Ajeyas-MacBook-Pro.local>
2016-12-14 22:48:13 -0500

    updated results for word2vec using Knn

--- src/Word2VecRepresentation.py
+++ src/Word2VecRepresentation.py
@@ -158,7 +158,7 @@ def examine_model():
 
 
 def run_experiment():
-    for i in [1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]:
+    for i in [35]:
         # classifier = LogisticRegression()
         classifier = KNeighborsClassifier(n_neighbors=i)
         # classifier = SVC()
@@ -169,14 +169,14 @@ def run_experiment():
         # tokenizer = BigramTokenizer()
 
         # corpus = ReviewPolarityCorpus(tokenizer)
-        # corpus = ImdbCorpus(tokenizer)
-        corpus = SubjectivityCorpus(tokenizer)
+        corpus = ImdbCorpus(tokenizer)
+        # corpus = SubjectivityCorpus(tokenizer)
 
         model = get_model(corpus, number_of_features)
 
-        # accuracy = evaluate(model, corpus, number_of_features, classifier)
-        scores = kFoldCrossValidate(10, model, corpus, number_of_features, classifier)
-        print("{0}, {1}".format(i), scores)
+        scores = evaluate(model, corpus, number_of_features, classifier)
+        # scores = kFoldCrossValidate(10, model, corpus, number_of_features, classifier)
+        print("{0}, {1}".format(i, scores))
 
 
 def visualize():
@@ -233,5 +233,5 @@ def visualize_words():
 
 # visualize()
 # examine_model()
-# run_experiment()
-visualize_words()
+run_experiment()
+# visualize_words()
